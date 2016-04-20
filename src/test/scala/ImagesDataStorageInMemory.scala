3a493b1bd0562c062cb269a6df0b4e967034e0bb
Aleksei <daron666@yandex.ru>
2016-04-20 12:12:26 +0400

    simple tests for post get and delete

--- src/test/scala/ImagesDataStorageInMemory.scala
+++ src/test/scala/ImagesDataStorageInMemory.scala
@@ -3,46 +3,45 @@ import java.util.UUID
 import blobs.BlobId
 import images.ImagesDataStorage
 import images.protocol.Image.Id
-import images.protocol.{Image, ImageRendered, ImagesFilter}
+import images.protocol.{ Image, ImageRendered, ImagesFilter }
 
 import scala.collection.concurrent.TrieMap
 import scala.concurrent.Future
 
 object ImagesDataStorageInMemory extends ImagesDataStorage {
-  private val storage: TrieMap[Id, Image] = TrieMap.empty[Id,Image]
+  private val storage: TrieMap[Id, Image] = TrieMap.empty[Id, Image]
 
-
-  private def getFilter(filter: ImagesFilter): Image => Boolean = {
-    val blobFilter: Image => Boolean = filter.blobId match {
-      case Some(blobId) => _.blobId == blobId
-      case None => _ => true
+  private def getFilter(filter: ImagesFilter): Image ⇒ Boolean = {
+    val blobFilter: Image ⇒ Boolean = filter.blobId match {
+      case Some(blobId) ⇒ _.blobId == blobId
+      case None         ⇒ _ ⇒ true
     }
 
-    val userIdFilter: Image => Boolean = filter.userId match {
-      case Some(userId) => _.userId == userId
-      case None => _ => true
+    val userIdFilter: Image ⇒ Boolean = filter.userId match {
+      case Some(userId) ⇒ _.userId == userId
+      case None         ⇒ _ ⇒ true
     }
 
-    image: Image => blobFilter(image) && userIdFilter(image)
+    image: Image ⇒ blobFilter(image) && userIdFilter(image)
   }
 
   override def get(imageId: Id): Future[Image] = storage.get(imageId) match {
-    case Some(image) => Future.successful(image)
-    case None => Future.failed(new NoSuchElementException)
+    case Some(image) ⇒ Future.successful(image)
+    case None        ⇒ Future.failed(new NoSuchElementException)
   }
 
   override def count(filter: ImagesFilter): Future[Int] = Future.successful(storage.values.count(getFilter(filter)))
 
   override def delete(imageId: Id): Future[Boolean] = {
     storage.remove(imageId) match {
-      case Some(x) => Future.successful(true)
-      case None => Future.successful(false)
+      case Some(x) ⇒ Future.successful(true)
+      case None    ⇒ Future.successful(false)
     }
   }
 
-  override def save(image: Image): Future[Image] = storage.put(UUID.randomUUID().toString, image) match {
-    case Some(im) => Future.successful(im)
-    case None => Future.failed(new Exception)
+  override def save(image: Image): Future[Image] = storage.put(image.id, image) match {
+    case Some(im) ⇒ Future.failed(new Exception)
+    case None     ⇒ Future.successful(image)
   }
 
   override def query(filter: ImagesFilter, offset: Int, limit: Int): Future[Seq[Image]] =
--- src/test/scala/ImagesHandlerSpec.scala
+++ src/test/scala/ImagesHandlerSpec.scala
@@ -1,28 +1,114 @@
-import java.nio.file.{FileSystems, Paths}
+import java.io.File
+import java.nio.file.Files
+import java.util.concurrent.TimeUnit
 
-import akka.http.scaladsl.server._
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.{ MediaTypes, _ }
+import akka.http.scaladsl.model.headers.{ Authorization, OAuth2BearerToken }
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.testkit.{ RouteTestTimeout, ScalatestRouteTest }
+import akka.stream.scaladsl.Source
 import blobs.FilesService
-import images.{ImagesHandler, ImagesService}
+import images.protocol.ImagesError
+import images.{ ImagesHandler, ImagesService }
+import org.scalatest.concurrent.{ Eventually, ScalaFutures }
+import org.scalatest._
+import org.scalatest.time.{ Seconds, Span }
+import play.api.libs.json.JsObject
+import utils.http.json.PlayJsonSupport
 import utils.http.protocol.ApiError.AuthorizationFailedError
 
-class ImagesHandlerSpec {
+import scala.concurrent.duration.FiniteDuration
+
+class ImagesHandlerSpec extends WordSpec with ScalatestRouteTest with Matchers with ScalaFutures with TryValues
+    with OptionValues with BeforeAndAfter with Eventually with PlayJsonSupport {
+
+  implicit override val patienceConfig = PatienceConfig(timeout = Span(5000, Seconds))
+  implicit val routeTimeout = RouteTestTimeout(FiniteDuration(5000, TimeUnit.SECONDS))
 
   import utils.http.ApiErrorHandler._
 
-
   private val imagesService = new ImagesService(ImagesDataStorageInMemory, new FilesService(System.getProperty("java.io.tmpdir")))
 
-  lazy val route = Route.seal(new ImagesHandler(imagesService){
+  lazy val route = Route.seal(new ImagesHandler(imagesService) {
     val userAware: Directive1[Option[String]] =
       optionalHeaderValueByName("Authorization")
-        .map(_.filter(_.startsWith("Id ")).map(_.stripPrefix("Id ")))
-
+        .map(_.filter(_.startsWith("Bearer ")).map(_.stripPrefix("Bearer ")))
 
     def userStringIdRequired: Directive1[String] = userAware.flatMap {
-      case Some(id) => provide(id)
-      case None => failWith(AuthorizationFailedError)
+      case Some(id) ⇒ provide(id)
+      case None     ⇒ failWith(AuthorizationFailedError)
     }
   }.route)
+
+  s"Image handler should" should {
+    "upload image and delete it after" in {
+
+      val tokenHeader = Authorization(OAuth2BearerToken("userSuperPuperUser"))
+      val image = new File(getClass.getResource("/picture.jpg").toURI)
+      val entity = createRequestEntityWithFile(image)
+
+      val imageJson = Post("/images").withEntity(entity).withHeaders(tokenHeader) ~> route ~> check {
+        status should be(StatusCodes.Created)
+        responseAs[JsObject]
+      }
+
+      val imageIdOpt = (imageJson \ "id").asOpt[String]
+      imageIdOpt shouldBe defined
+      val imageId = (imageJson \ "id").as[String]
+
+      Delete(s"/images/$imageId").withHeaders(tokenHeader) ~> route ~> check {
+        status should be(StatusCodes.NoContent)
+      }
+
+      imagesService.getImage(imageId).failed.futureValue shouldBe ImagesError.NotFound
+    }
+
+    "show 404 if image already deleted" in {
+
+      val tokenHeader = Authorization(OAuth2BearerToken("userSuperPuperUser"))
+      val image = new File(getClass.getResource("/picture.jpg").toURI)
+      val entity = createRequestEntityWithFile(image)
+
+      val imageJson = Post("/images").withEntity(entity).withHeaders(tokenHeader) ~> route ~> check {
+        status should be(StatusCodes.Created)
+        responseAs[JsObject]
+      }
+
+      val imageIdOpt = (imageJson \ "id").asOpt[String]
+      imageIdOpt shouldBe defined
+      val imageId = (imageJson \ "id").as[String]
+
+      Get(s"/images/$imageId").withHeaders(tokenHeader) ~> route ~> check {
+        status should be(StatusCodes.OK)
+      }
+
+      Delete(s"/images/$imageId").withHeaders(tokenHeader) ~> route ~> check {
+        status should be(StatusCodes.NoContent)
+      }
+
+      Get(s"/images/$imageId").withHeaders(tokenHeader) ~> route ~> check {
+        status should be(StatusCodes.NotFound)
+      }
+
+      imagesService.getImage(imageId).failed.futureValue shouldBe ImagesError.NotFound
+    }
+  }
+
+  private def createRequestEntityWithFile(file: File): RequestEntity = {
+    require(file.exists())
+    val formData =
+      Multipart.FormData(
+        Source.single(
+          Multipart.FormData.BodyPart(
+            "data",
+            HttpEntity(ContentType(MediaTypes.`image/jpeg`), Files.readAllBytes(file.toPath)),
+            Map("filename" → file.getName)
+          )
+        )
+      )
+    Marshal(formData).to[RequestEntity].futureValue
+  }
 
 }
